dd6ec58042014da11bc538a8333f89eab8157482
Meow <Meow@DESKTOP-5LQSSIC>
2021-03-02 15:58:15 +0500

    fixCarousel

--- VDCompanyMVC/Models/Conf.cs
+++ VDCompanyMVC/Models/Conf.cs
@@ -7,8 +7,8 @@ namespace VDCompanyMVC.Models
 {
     public static class Conf
     {                                                            //User=vddbadmin;Password=4Thehorde!
-        public static string ConnectDb { get; private set; } = "Server=localhost;Database=u0967433_vd;User=u0967_vd;Password=$lI2bg26;";
-        public static string DBChat { get; private set; } = "Server=localhost;Database=u0967433_vd_chat;User=u0967_vd;Password=$lI2bg26;";
+        public static string ConnectDb { get; private set; } = "Server=localhost;Database=u0967433_afc;User=u0967_vd;Password=$lI2bg26;";
+        public static string DBChat { get; private set; } = "Server=localhost;Database=u0967433_afc_chat;User=u0967_vd;Password=$lI2bg26;";
     }
     public static class MessagesUser
     {
