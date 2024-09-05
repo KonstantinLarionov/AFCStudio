a39655c08bd817c2970e02856885377ecd5a880c
CvCvCvv <decomen123@gmail.com>
2024-09-05 13:21:04 +0500

    clear

--- VDCompanyMVC/Controllers/HomeController.cs
+++ VDCompanyMVC/Controllers/HomeController.cs
@@ -1,129 +1,30 @@
 ﻿using AFCStudio.Models.Helpers;
-using Microsoft.AspNetCore.Http;
+
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
+
 using VDCompany.Controllers;
-using VDCompanyMVC.Models.Entitys;
-using VDCompanyMVC.Models.Objects;
-using VDCompanyMVC.Models.Secur;
 
 namespace VDCompanyMVC.Controllers
 {
-    public class HomeController: Controller
+    public class HomeController : Controller
     {
-        private ChatContext chat = new ChatContext(new DbContextOptions<ChatContext>());
-        private StartContext db = new StartContext(new DbContextOptions<StartContext>());
+
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public string SendFeedBack(string name, string email, string message)
+        public string SendFeedBack( string name, string email, string message )
         {
             try
             {
-                string content = "<p><font size = \"3\" face = \"Source Serif Pro\">Новое сообщение через обратную связь <br> <strong>Имя:</strong> "+name+" <br> <strong>Обратный Email:</strong> "+email+" <br> <strong>Сообщение:</strong><br> "+message+" </font></p>";
-                Letters.Send("afc.studio@yandex.ru", "Обратная связь.", content).GetAwaiter().GetResult();
+                string content = "<p><font size = \"3\" face = \"Source Serif Pro\">Новое сообщение через обратную связь <br> <strong>Имя:</strong> " + name + " <br> <strong>Обратный Email:</strong> " + email + " <br> <strong>Сообщение:</strong><br> " + message + " </font></p>";
+                Letters.Send( "afc.studio@yandex.ru", "Обратная связь.", content ).GetAwaiter().GetResult();
                 return JsonAnswer.H_Success();
-                //return "{ \"status\": \"success\" }";
             }
             catch
             {
                 return JsonAnswer.H_Error();
-                //return "{ \"status\": \"error\" }";
-            }
-        }
-        [HttpPost]
-        public string GetUsers(string token = "-")
-        {
-            if (token != "69ae97f121aa0daf0a0c225a73c16e5782fa448")
-                return "[]";
-            var users = chat.Users.OrderByDescending(d => d.Date).ToList();
-            return JsonSerializer.Serialize(users);
-        }
-        [HttpPost]
-        public string GetHistory(string login, string token = "-")
-        {
-            var user = chat.Users.Where(x => x.Login == login).FirstOrDefault();
-            try
-            {
-                var dialog = chat.Dialogs.Where(x => x.Identity == user.UserIdentity).Include(x => x.Messages).ThenInclude(v => v.User).FirstOrDefault();
-                dialog.Messages.ForEach(x =>
-                {
-                    x.User.IP = null;
-                    x.User.Password = null;
-                    x.User.Role = 0;
-                });
-                return JsonSerializer.Serialize(dialog.Messages);
-            }
-            catch { return "[]"; }
-        }
-        [HttpPost]
-        public string Gallery(List<IFormFile> files, int case_id)
-        {
-            try
-            {
-                var _case = db.Cases.Where(f => f.Id == case_id).Include(d => d.Docs).FirstOrDefault();
-                foreach (var file in files)
-                {
-                    var ext = file.FileName.Split('.').Last();
-                    TypeDoc type;
-                    switch (ext)
-                    {
-                        case ".doc":
-                            type = TypeDoc.WORD;
-                            break;
-                        case ".xlsx":
-                            type = TypeDoc.XLC;
-                            break;
-                        case ".xls":
-                            type = TypeDoc.XLC;
-                            break;
-                        case ".pdf":
-                            type = TypeDoc.PDF;
-                            break;
-                        case ".png":
-                            type = TypeDoc.IMG;
-                            break;
-                        case ".bmp":
-                            type = TypeDoc.IMG;
-                            break;
-                        case ".jpg":
-                            type = TypeDoc.IMG;
-                            break;
-                        case ".jpeg":
-                            type = TypeDoc.IMG;
-                            break;
-                        default:
-                            type = TypeDoc.NONE;
-                            break;
-                    }
-                    _case.Docs.Add(new Doc
-                    {
-                        URL = file.FileName,
-                        Type = type,
-                        DateAdd = DateTime.Now
-                    });
-                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", file.FileName);
-                    using (var stream = new FileStream(path, FileMode.Create))
-                    {
-                        file.CopyTo(stream);
-                    }
-                }
-                db.SaveChanges();
-                return JsonAnswer.H_Gallery_FilesUploaded(files.Count.ToString());
-                //return "{\"status\":\"success\", \"data\":\"Загружено файлов: " + files.Count.ToString() + "\"}";
-            }
-            catch (Exception exp)
-            {
-                return JsonAnswer.H_Gallery_Error(exp.ToString());
-                //return "{\"status\":\"error\", \"data\": \"" + exp.ToString() + "\"}";
             }
         }
     }
--- VDCompanyMVC/Startup.cs
+++ VDCompanyMVC/Startup.cs
@@ -1,97 +1,91 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using Newtonsoft.Json;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using VDCompanyMVC.Hubs;
-using VDCompanyMVC.Models;
-using VDCompanyMVC.Models.Entitys;
 
 namespace VDCompanyMVC
 {
     internal static class Extensions
     {
-        public static string ToJson(this object obj)
+        public static string ToJson( this object obj )
         {
-            return JsonConvert.SerializeObject(obj);
+            return JsonConvert.SerializeObject( obj );
         }
-        public static T ToObject<T>(this string json)
+        public static T ToObject<T>( this string json )
         {
-            return JsonConvert.DeserializeObject<T>(json);
+            return JsonConvert.DeserializeObject<T>( json );
         }
-        public static string Get(this string str, string nameParam)
+        public static string Get( this string str, string nameParam )
         {
-            var arrayStr = str.Split('-');
-            var selectSection = arrayStr.FirstOrDefault(x => x.Contains(nameParam));
-            if (!string.IsNullOrEmpty(selectSection))
+            var arrayStr = str.Split( '-' );
+            var selectSection = arrayStr.FirstOrDefault( x => x.Contains( nameParam ) );
+            if ( !string.IsNullOrEmpty( selectSection ) )
             {
-                return selectSection.Replace(nameParam, string.Empty).Trim();
+                return selectSection.Replace( nameParam, string.Empty ).Trim();
             }
             return string.Empty;
         }
-        public static int ToInt(this string str)
+        public static int ToInt( this string str )
         {
             try
             {
-                return Convert.ToInt32(str);
+                return Convert.ToInt32( str );
             }
             catch
             {
                 return 0;
             }
         }
-        public static double ToDouble(this string str)
+        public static double ToDouble( this string str )
         {
             try
             {
-                return Convert.ToDouble(str.Replace(".", ","));
+                return Convert.ToDouble( str.Replace( ".", "," ) );
             }
             catch
             {
                 return 0;
             }
         }
-        public static string ToStringDF(this double number)
+        public static string ToStringDF( this double number )
         {
             try
             {
-                return Convert.ToString(number).Replace(",", ".");
+                return Convert.ToString( number ).Replace( ",", "." );
             }
             catch
             {
                 return "";
             }
         }
-        public static string ToStringIF(this int number)
+        public static string ToStringIF( this int number )
         {
             try
             {
                 var countV = number.ToString().Count();
-                switch (countV)
+                switch ( countV )
                 {
                     case 10:
-                        return number.ToString("# ### ### ###");
+                        return number.ToString( "# ### ### ###" );
                     case 9:
-                        return number.ToString("### ### ###");
+                        return number.ToString( "### ### ###" );
                     case 8:
-                        return number.ToString("## ### ###");
+                        return number.ToString( "## ### ###" );
                     case 7:
-                        return number.ToString("# ### ###");
+                        return number.ToString( "# ### ###" );
                     case 6:
-                        return number.ToString("### ###");
+                        return number.ToString( "### ###" );
                     case 5:
-                        return number.ToString("## ###");
+                        return number.ToString( "## ###" );
                     case 4:
-                        return number.ToString("# ###");
+                        return number.ToString( "# ###" );
                     default:
                         return number.ToString();
                 }
@@ -104,69 +98,54 @@ namespace VDCompanyMVC
     }
     public class Startup
     {
-        private static readonly StartContext db = new StartContext(new DbContextOptions<StartContext>());
         internal static Action<string> Logs = null;
-        public Startup(IConfiguration configuration)
+        public Startup( IConfiguration configuration )
         {
             Configuration = configuration;
             //Logs += Loger;
         }
-        public void Loger(string message)
+        public void Loger( string message )
         {
             var main_path = Environment.CurrentDirectory + @"\Logs\" + DateTime.Now.Date.Day.ToString() + "." + DateTime.Now.Date.Month.ToString() + @"\";
-            if (Directory.Exists(main_path))
+            if ( Directory.Exists( main_path ) )
             {
                 var file_path = $"{main_path}LOGAT{DateTime.Now.Hour.ToString()}-{DateTime.Now.Minute.ToString()}.txt";
 
-                File.AppendAllText(file_path, message);
+                File.AppendAllText( file_path, message );
             }
             else
             {
-                Directory.CreateDirectory(main_path);
-                Loger(message);
+                Directory.CreateDirectory( main_path );
+                Loger( message );
             }
         }
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices( IServiceCollection services )
         {
             services.AddControllersWithViews();
             services.AddSignalR();
             services.AddDistributedMemoryCache();
             services.AddHttpContextAccessor();
             services.AddControllers();
-            services.AddDbContextPool<StartContext>(
-               options => options.UseMySql(Conf.ConnectDb,
-               mySqlOptions =>
-                 {
-                     mySqlOptions.ServerVersion(new Version(5, 6, 45), ServerType.MySql);
-                 }
-           ));
-            services.AddDbContextPool<ChatContext>(
-            options => options.UseMySql(Conf.DBChat,
-                mySqlOptions =>
-                {
-                    mySqlOptions.ServerVersion(new Version(5, 6, 45), ServerType.MySql);
-                }
-        ));
             services.AddDistributedMemoryCache();
-            services.AddSession(options =>
+            services.AddSession( options =>
             {
-                options.IdleTimeout = TimeSpan.FromSeconds(100000);
+                options.IdleTimeout = TimeSpan.FromSeconds( 100000 );
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
-            });
+            } );
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure( IApplicationBuilder app, IWebHostEnvironment env )
         {
-            if (env.IsDevelopment())
+            if ( env.IsDevelopment() )
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler( "/Home/Error" );
                 app.UseHsts();
             }
 
@@ -179,15 +158,12 @@ namespace VDCompanyMVC
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints( endpoints =>
             {
-                endpoints.MapHub<ChatHub>("/chat");
-                endpoints.MapHub<CaseHub>("/caseHub");
-                endpoints.MapHub<MyHub>("/MyHub");
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+                    pattern: "{controller=Home}/{action=Index}/{id?}" );
+            } );
         }
     }
 }
