aaa3cc128dde4c7e78d2136cf02ee4bce064db2e
Konstantin Larionov <konstantin.l@octopays.io>
2025-12-01 15:04:16 +0700

    rehtml

--- VDCompanyMVC/Controllers/HomeController.cs
+++ VDCompanyMVC/Controllers/HomeController.cs
@@ -13,13 +13,17 @@ namespace VDCompanyMVC.Controllers
         {
             return View();
         }
+        public IActionResult Competencies()
+        {
+            return View();
+        }
         [HttpPost]
         public string SendFeedBack( string name, string email, string message )
         {
             try
             {
                 string content = "<p><font size = \"3\" face = \"Source Serif Pro\">Новое сообщение через обратную связь <br> <strong>Имя:</strong> " + name + " <br> <strong>Обратный Email:</strong> " + email + " <br> <strong>Сообщение:</strong><br> " + message + " </font></p>";
-                Letters.Send( "afc.studio@yandex.ru", "Обратная связь.", content ).GetAwaiter().GetResult();
+                Letters.Send( "afc.studio@yandex.ru", "Feedback.", content ).GetAwaiter().GetResult();
                 return JsonAnswer.H_Success();
             }
             catch
